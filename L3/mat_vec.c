fafe1424a0d7daa61334d260a8938f32390d98c4
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-02-03 10:09:24 -0500

    prueba

--- L3/mat_vec.c
+++ L3/mat_vec.c
@@ -4,7 +4,7 @@ void mat_vec(int *A, int *B, int *C, int N)
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
             C[i*N+j]=0;
-            for(int k = 0;k<N;k++){
+            for(int k = 0;k<N;k++){a
             C[i*N+j] += A[i*N + k]*B[k*N+j];
             }
         }
