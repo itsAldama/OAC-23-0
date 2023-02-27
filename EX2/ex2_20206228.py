4abff58b6a011de4e5579dff371e2331f0bf6bbb
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-02-26 19:29:44 -0500

    pool_fin

--- EX2/ex2_20206228.py
+++ EX2/ex2_20206228.py
@@ -3,28 +3,26 @@ from math import floor, sqrt, ceil
 from multiprocessing import Pool, Queue, Process
 
 n = 2345678911111111
-    
-def verificar_es_primo(numero):
+# n = 2000
 
-    es_primo = True
+def verificar_es_primo_sinc(numero):
 
     for i in range(2, floor(sqrt(numero)) + 1):
         if numero % i == 0:
-            es_primo = False
-            return es_primo
+            return False
     
-    return es_primo
+    return True
 
-def verificar_primo(nums_evaluar: list):
+def verificar_es_primo_chunk(ini, fin):
 
-    for i in nums_evaluar:
+    for i in range(ini, fin):
         if n % i == 0:
             return False
     return True
 
 def dividir_numeros_inicio(n, jump):
     for i in range(2, n, jump):
-        yield list(range(i, i+jump))
+        yield i
 
 def dividir_numeros_fin(n, jump):
     for i in range(2, n, jump):
@@ -34,43 +32,36 @@ def verificar_es_primo_multi(numero):
 
     ultimo = floor(sqrt(numero)) + 1
     
-    num_procc = 24
+    num_procc = 8
 
-    # p = Pool(processes=num_procc)
-
-    # // redondea hacia abajo
     jump = ceil((ultimo-2)/num_procc)
+
+    inicio = list(dividir_numeros_inicio(ultimo, jump))
+    final = list(dividir_numeros_fin(ultimo, jump))
+
+    chunks = zip(inicio, final)
     
-    chunks = list(dividir_numeros_inicio(ultimo, jump))
-    # final = list(dividir_numeros_fin(n, jump))
-    print("chunks")
-    # print(chunks)
+    print(inicio)
+    print(final)
     with Pool(processes=num_procc) as pool:
-        results = pool.map(verificar_primo, chunks)
+        results = pool.starmap(verificar_es_primo_chunk, chunks)
     
     if False in results:
         return False
     else:
         return True
 
-    # p1 = Process(target=verificar_primo, args=(2, ultimo//2, numero, ))
-    # p2 = Process(target=verificar_primo, args=(ultimo//2, ultimo, numero, ))
-    # p1.start()
-    # p2.start()
-    # p1.join()
-    # p2.join()
-
 if __name__ == "__main__":
     # n = 2345678911111111
     # n = 2382847902741
 
-    # tic = time.perf_counter()
-    # es_primo_sin = verificar_es_primo(n)
-    # toc = time.perf_counter()
-    # sin_multiprocess_time = toc-tic
+    tic = time.perf_counter()
+    es_primo_sinc = verificar_es_primo_sinc(n)
+    toc = time.perf_counter()
+    sin_multiprocess_time = toc-tic
 
-    # print()
-    # print("Tiempo de ejecución sin multiprocessing:", sin_multiprocess_time)
+    print()
+    print("Tiempo de ejecución sin multiprocessing:", sin_multiprocess_time)
 
     tic = time.perf_counter()
     es_primo_con = verificar_es_primo_multi(n)
@@ -79,6 +70,6 @@ if __name__ == "__main__":
     print("Es primo?", es_primo_con)
     print("Tiempo de ejecución con multiprocessing:", con_multiprocess_time, end="\n\n")
     
-    # assert(es_primo_sin==es_primo_con)
-    # print("Prueba assert pasada correctamente los resultados son iguales", end="\n\n")
-    # print(f"Speed Up sin_multiprocess_time/con_multiprocess_time: {sin_multiprocess_time/con_multiprocess_time: 0.2f}", end="\n\n")
+    assert(es_primo_sinc==es_primo_con)
+    print("PRUEBA ASSERT PASADA CORRECTAMENTE, LOS RESULTADOS SON IGUALES", end="\n\n")
+    print(f"Speed Up sin_multiprocess_time/con_multiprocess_time: {sin_multiprocess_time/con_multiprocess_time: 0.2f}", end="\n\n")
