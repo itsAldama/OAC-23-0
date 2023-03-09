067c14d2956a1959ee3876f1d61b0308806272af
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-03-09 17:13:56 -0500

    lab3 completo

--- L3/lib.c
+++ L3/lib.c
@@ -1,5 +1,6 @@
 // Para generar el archivo .so, debe escribir en el terminal lo siguiente:
-// gcc -shared lib.c -o lib.so 
+// gcc -shared lib.c -o lib.so
+
 void mat_vec(int *A, int *B, int *C, int N) 
 {
     int tmp = 0;
